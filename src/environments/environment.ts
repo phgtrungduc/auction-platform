24195277caa35aded5fdd1caa8a235a813eaf617
vtlam1406 <lamvt1406@gmail.com>
2026-04-05 12:33:48 +0700

    no message

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //apiUrl: 'https://api.auction-platform.com/api',
-  apiUrl: 'http://localhost:5204/api',
+  apiUrl: 'https://api.auction-platform.com/api',
+  // apiUrl: 'http://localhost:5204/api',
 };
